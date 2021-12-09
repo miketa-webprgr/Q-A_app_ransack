a2657e3820d6b9d0e15a59b58c109cd88b15c207
keit1722 <keit1722@gmail.com>
2021-12-10 04:19:38 +0900

    ログイン機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users, except: :destroy
+  root to: 'questions#index'
   resources :questions
 end
