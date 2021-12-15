d643067b6952844201296bff0bb71d9b05de517c
keit1722 <keit1722@gmail.com>
2021-12-16 08:13:37 +0900

    アバター登録機能を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,6 +41,6 @@ class UsersController < ApplicationController
   def user_params
     params
       .require(:user)
-      .permit(:name, :email, :password, :password_confirmation, :admin)
+      .permit(:name, :email, :password, :password_confirmation, :admin, :image)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_one_attached :image
+
   has_secure_password
 
   validates :name, presence: true, uniqueness: true
