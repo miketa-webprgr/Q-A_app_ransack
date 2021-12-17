b71c03937b8500497300cab28a4d22539f366e57
keit1722 <keit1722@gmail.com>
2021-12-17 09:52:56 +0900

    questionsテーブルにsolvedカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_15_214820) do
+ActiveRecord::Schema.define(version: 2021_12_17_004553) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_12_15_214820) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.boolean "solved", default: false, null: false
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
