910165fd1f21449fbabcdc03c4ed00753b67c5cf
keit1722 <keit1722@gmail.com>
2021-12-18 08:37:59 +0900

    質問の解決・未解決の絞り込み機能を作成

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,7 +1,20 @@
 class Admin::QuestionsController < Admin::BaseController
   def index
     @q = Question.ransack(params[:q])
-    @questions = @q.result(distinct: true).page(params[:page]).per(5)
+
+    if params[:solved] == 'true'
+      @questions =
+        @q.result(distinct: true).where(solved: true).page(params[:page]).per(5)
+    elsif params[:solved] == 'false'
+      @questions =
+        @q
+          .result(distinct: true)
+          .where(solved: false)
+          .page(params[:page])
+          .per(5)
+    else
+      @questions = @q.result(distinct: true).page(params[:page]).per(5)
+    end
     @users = User.all
   end
 
